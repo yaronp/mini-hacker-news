fa9801000134073a9cfb3580df409380d49ca0a8
yaronp <Mydlpk236>
2017-07-23 19:22:12 +0300

    skeleton added

---
+++ web/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/posts/<id>', methods=['POST'])
+def api_posts(id=None):
+    print post_id
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+
+
+@app.route('/upvote', methods=['POST'])
+def api_up_vote():
+    id = request.args.get('id', '')
+    print id
+    pass
+
+
+@app.route('/downvote', methods=['POST'])
+def api_down_vote():
+    id = request.args.get('id', '')
+    print id
+    pass
+
+
+@app.route('posts', methods=['GET'])
+def api_top_list():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ web/dal.py
@@ -0,0 +1,31 @@
+from unqlite import UnQLite
+
+
+def dal_create(post_text):
+    print post_text
+    pass
+
+
+def dal_update(post_id, update_text):
+    print post_id
+    print update_text
+    pass
+
+
+def dal_up_vote(post_id):
+    print post_id
+    pass
+
+
+def dal_down_vote(post_id):
+    print post_id
+    pass
+
+
+def dal_top_list(num_of_posts=10):
+    print num_of_posts
+    pass
+
+
+def dal_open_db():
+    pass
