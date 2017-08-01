9444e65593ff953b697e180ef0b4c0ef484d4175
Yaron Pdut <yaron.pdut@nice.com>
2017-08-01 08:42:41 +0300

    Fixed in REST

--- web/app.py
+++ web/app.py
@@ -13,10 +13,32 @@ def http_error(msg, code):
     return json.dumps({msg: True}), code, {'ContentType': 'application/json'}
 
 
+def get_post(id):
+    dal = Dal()
+
+    r = dal.get(id)
+    response = {"id": r['_id'], "date": r['date'], "post": r['post']}
+    return json.dumps(response), 200, {'ContentType': 'application/json'}
+
+
+def create_post(post_text):
+    dal = Dal()
+    if not dal.create(post_text):
+        return http_error('error while inserting record to storage', 500)
+    else:
+        return http_error('success', 200)
+
+
 def register_routes(app):
-    @app.route('/v0/post', methods=['POST'])
+    @app.route('/v0/post', methods=['POST', 'GET'])
     def api_posts():
         post_id = request.args.get('id')
+
+        if request.method == 'GET':
+            if post_id is None:
+                return http_error('No post id in args', 400)
+            return get_post(post_id)
+
         content = request.get_json(silent=True)
         if request.data is None or type(content) is not dict:
             return http_error('empty or wrong type body (json body type?)', 400)
@@ -24,27 +46,14 @@ def register_routes(app):
         if post_text is None:
             return http_error('post field not found in body', 400)
 
-        dal = Dal()
         if post_id is None:
-            if not dal.create(post_text):
-                return http_error('error while inserting record to storage', 500)
+            return create_post(post_text)
         else:
+            dal = Dal()
             if not dal.update(post_id, post_text):
                 return http_error('error while inserting record to storage', 500)
 
         return http_error('success', 200)
-
-    @app.route('/v0/post', methods=['GET'])
-    def api_post_get():
-        post_id = request.args.get('id')
-        dal = Dal()
-
-        r = dal.get(post_id)
-        response = {"id": r['_id'],
-                    "date": r['date'],
-                    "post": r['post']
-                    }
-        return json.dumps(response), 200, {'ContentType': 'application/json'}
 
     @app.route('/v0/upvote', methods=['POST'])
     def api_up_vote():
@@ -74,6 +83,6 @@ def register_routes(app):
 
 
 if __name__ == '__main__':
-    app = create_app()
-    register_routes(app)
-    app.run(debug=True, host='0.0.0.0')
+    the_app = create_app()
+    register_routes(the_app)
+    the_app.run(debug=True, host='0.0.0.0')
